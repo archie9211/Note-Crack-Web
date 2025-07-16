4c498808e792821d2ae75f5583242a3ddb1321d1
Nageen Chand <nageen523@gmail.com>
2025-07-16 23:23:15 +0530

    atttept 1 on rendering mathJax and chemistry formulae - WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,16 @@ import tailwindcss from "@tailwindcss/vite";
 import cloudflare from "@astrojs/cloudflare";
 
 import AstroPWA from "@vite-pwa/astro";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
       output: "server",
+      markdown: {
+            remarkPlugins: [remarkMath],
+            rehypePlugins: [rehypeKatex],
+      },
 
       vite: {
             plugins: [tailwindcss()],
